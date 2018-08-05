83c239e5fae070f746ee419ab74032196a4b6b46
gusarov2k <motoface@yandex.ru>
2018-08-05 20:13:09 +0300

    add class Train

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180804235137) do
+ActiveRecord::Schema.define(version: 20180805170857) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "trains", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
