37feb1fa256d726759befff3ff1325ec7abf680e
gusarov2k <motoface@yandex.ru>
2018-08-05 22:23:07 +0300

    add new private method

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -10,4 +10,16 @@ class TrainsController < ApplicationController
 	def new
 		@train = Train.new
 	end
+
+	def create
+		@train = Train.new(train_params)
+	end
+
+
+	private
+
+	def train_params
+		params.require(:train).permit(:number)
+		
+	end
 end
