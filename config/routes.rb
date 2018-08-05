71318d84adc805ff5635feb948d92b34f3a2ff6c
gusarov2k <motoface@yandex.ru>
2018-08-05 20:14:12 +0300

    create routes for trains

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
+  resourses :trains
   resources :railway_stations
- root 'welcome#index'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
