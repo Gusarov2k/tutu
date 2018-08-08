bfe352937196c01e54277a0d839a2771cdf98c87
gusarov2k <motoface@yandex.ru>
2018-08-08 17:40:25 +0300

    add methods in controller

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,6 +1,24 @@
 class RoutesController < ApplicationController
+	before_action :set_route, only: [:show, :edit, :update, :destroy]
 
 	def index
-		
+		@routes = Route.all	
+	end
+
+	def new
+		@route = Route.new
+	end
+
+
+
+
+	private
+
+	def set_route
+		@train = Train.find(params[:id])
+	end
+
+	def route_params
+		params.require(:train).permit(:number)
 	end
 end
