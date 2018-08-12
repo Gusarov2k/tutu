5704985f21769f0338271a8445da31603f7d16f3
gusarov2k <motoface@yandex.ru>
2018-08-12 21:47:18 +0300

    bundle exec rake db:migrate and fix bag

--- db/migrate/20180812184401_create_stations_routes.rb
+++ db/migrate/20180812184401_create_stations_routes.rb
@@ -2,7 +2,7 @@ class CreateStationsRoutes < ActiveRecord::Migration
   def change
     create_table :stations_routes do |t|
     	t.integer 	:station_id
-    	t.ineteger	:route_id
+    	t.integer	:route_id
     end
   end
 end
