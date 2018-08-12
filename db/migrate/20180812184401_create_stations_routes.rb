2763b80c15a0d8550e95c52df260122e3243c309
gusarov2k <motoface@yandex.ru>
2018-08-12 21:46:27 +0300

    add column for migration has many to many

--- db/migrate/20180812184401_create_stations_routes.rb
+++ db/migrate/20180812184401_create_stations_routes.rb
@@ -1,6 +1,8 @@
 class CreateStationsRoutes < ActiveRecord::Migration
   def change
     create_table :stations_routes do |t|
+    	t.integer 	:station_id
+    	t.ineteger	:route_id
     end
   end
 end
