388cdd41af02be2c6ccbb605256c6dbeb7f9d976
gusarov2k <motoface@yandex.ru>
2018-08-12 21:06:47 +0300

    change migration

--- db/migrate/20180812175004_add_railway_station_id_to_trains.rb
+++ db/migrate/20180812175004_add_railway_station_id_to_trains.rb
@@ -1,6 +1,6 @@
 class AddRailwayStationIdToTrains < ActiveRecord::Migration
   def change
   	#add_column :trains, :railway_station_id, :integer
-  	add_belongs_to :trains, :railway_stations
+  	add_belongs_to :trains, :railway_station
   end
 end
