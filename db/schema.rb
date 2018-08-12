c22e36842fd2cd80ff16f99a556fc00805b1f6ef
gusarov2k <motoface@yandex.ru>
2018-08-12 20:54:43 +0300

    bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180808141435) do
+ActiveRecord::Schema.define(version: 20180812175004) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180808141435) do
     t.string   "number"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "railway_stations_id"
   end
 
 end
