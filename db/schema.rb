368f800f2e9264402107d6bef86251235b7a7297
gusarov2k <motoface@yandex.ru>
2018-08-20 23:54:02 +0300

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180819144859) do
+ActiveRecord::Schema.define(version: 20180819153315) do
+
+  create_table "create_tickets_users", force: :cascade do |t|
+    t.integer "ticket_id"
+    t.integer "user_id"
+  end
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +32,11 @@ ActiveRecord::Schema.define(version: 20180819144859) do
   create_table "routes", force: :cascade do |t|
     t.string  "name"
     t.integer "current_train_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "trains", force: :cascade do |t|
