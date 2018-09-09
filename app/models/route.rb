cedab1170a3de36cd47f03007d060b074e669fb6
gusarov2k <motoface@yandex.ru>
2018-09-09 22:32:41 +0300

    cange associacion route

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,7 +3,7 @@ class Route < ActiveRecord::Base
 
 	has_many :railway_stations_routes
 	has_many :railway_stations, through: :railway_stations_routes
-	has_many :trains
+	has_one :train
 	# change
 
 	has_many :tickets
