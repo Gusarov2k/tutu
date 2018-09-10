1ab672f83a6aba42232f9c70bad7c087d0cf56cd
gusarov2k <motoface@yandex.ru>
2018-09-10 06:24:41 +0300

    add column in  migration create_carriage

--- db/migrate/20180910031802_create_carriages.rb
+++ db/migrate/20180910031802_create_carriages.rb
@@ -1,6 +1,9 @@
 class CreateCarriages < ActiveRecord::Migration
   def change
     create_table :carriages do |t|
+    	t.boolean		:type_carriage, default: false
+    	t.integer		:quant_up_seat
+    	t.integer		:quant_down_seat
     end
   end
 end
