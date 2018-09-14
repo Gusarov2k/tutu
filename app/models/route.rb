f66e74e3a37744e71affa58f0bb7c56d69d4c62a
gusarov2k <motoface@yandex.ru>
2018-09-14 06:17:42 +0300

    add validation in Route

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,7 @@
 class Route < ActiveRecord::Base
 
-	validates :name, presence: true 
+	validates :name, presence: true
+	validate :stations_count
 
 	has_many :railway_stations_routes
 	has_many :railway_stations, through: :railway_stations_routes
@@ -15,5 +16,11 @@ private
 
 	def set_name
 		self.name ||= "#{railway_stations.first.title} - #{railway_stations.last.title}"
+	end
+
+	def stations_count
+		if railway_stations.size < 2
+			errors.add(:base, "Route should contain at least 2 stations")
+		end
 	end
 end
