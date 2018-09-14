2ebc3a2548d933e54d5f9407f9781d5464a2513f
gusarov2k <motoface@yandex.ru>
2018-09-14 03:31:56 +0300

    for add multiple form second part

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,4 +1,4 @@
 class Carriage < ActiveRecord::Base
 	has_many :trains
-	accepts_nested_attributes_for :trains
+	accepts_nested_attributes_for :trains, :reject_if => lambda { |a| a[:content].blank? }
 end
