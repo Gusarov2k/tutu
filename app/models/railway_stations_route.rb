7ddc06c5e2f1a2c3ec67759154c4b529fa26f0e8
gusarov2k <motoface@yandex.ru>
2018-09-14 06:10:29 +0300

    add validation

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -1,4 +1,8 @@
 class RailwayStationsRoute < ActiveRecord::Base
 	belongs_to :railway_station
 	belongs_to :route
+
+	# add validation on uniqueness row
+	validates :railway_station_id, uniqueness: true
+
 end
