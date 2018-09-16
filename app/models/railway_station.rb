a85ad70e97d8bc60a53771ee36235c915b9b4561
gusarov2k <motoface@yandex.ru>
2018-09-16 15:07:32 +0300

    add refactoring

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -6,13 +6,19 @@ class RailwayStation < ActiveRecord::Base
 	has_many :routes, through: :railway_stations_routes
 
 	def update_position(route, position)
-		station_route = railway_stations_routes.where(route: route).first
+		station_route = station_route(route)
 		station_route.update(position: position) if station_route
 	end
 
 	def position_in(route)
-		station_route = railway_stations_routes.where(route: route).first
-		station_route.try(:position)
+		station_route(route).try(:position)
+	end
+
+	protected
+
+	def station_route(route)
+		# мемоизация ||= т.е. если если какой то метод уже загрузил данный объект то данный метод будет только его возвращать
+		@station_route ||= railway_stations_routes.where(route: route).first
 		
 	end
 end
