fd71273683798db7086886f149547eddac62e8cc
gusarov2k <motoface@yandex.ru>
2018-09-16 15:01:25 +0300

    add method position_in

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -8,6 +8,11 @@ class RailwayStation < ActiveRecord::Base
 	def update_position(route, position)
 		station_route = railway_stations_routes.where(route: route).first
 		station_route.update(position: position) if station_route
+	end
+
+	def position_in(route)
+		station_route = railway_stations_routes.where(route: route).first
+		station_route.try(:position)
 		
 	end
 end
