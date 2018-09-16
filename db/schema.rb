881f79ace0680790c5de34782825719c398d0548
gusarov2k <motoface@yandex.ru>
2018-09-16 13:55:06 +0300

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180910033509) do
+ActiveRecord::Schema.define(version: 20180916105259) do
 
   create_table "carriages", force: :cascade do |t|
     t.boolean "type_carriage",   default: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180910033509) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+    t.integer "position"
   end
 
   create_table "routes", force: :cascade do |t|
